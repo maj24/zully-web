b147d9319303e41847a115921b61d783fe34029b
Maria Jose Patron <mariajose.patron@gmail.com>
2017-11-23 13:12:32 -0600

    Created toolbar component

--- src/components/EditorHeader.js
+++ src/components/EditorHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import userIcon from './../assets/images/user-icon.png';
 import {Button, Icon} from 'antd';
+import Toolbar from './Toolbar';
 
 class EditorHeader extends React.Component {
 
@@ -20,38 +21,7 @@ class EditorHeader extends React.Component {
               <p className="creator">Majo Patron</p>
             </div>
           </div>
-          <div id='toolbar'>
-            <select className="ql-header">
-              <option value="1"/>
-              <option value="2"></option>
-              <option selected></option>
-            </select>
-            <select className="ql-align">
-              <option value="center"></option>
-              <option value="right"></option>
-              <option value="justify"></option>
-              <option selected></option>
-            </select>
-            <button className="ql-bold"></button>
-            <button className="ql-italic"></button>
-            <button className="ql-underline"></button>
-            <button className="ql-strike"></button>
-            <select className="ql-color">
-              <option value="red"></option>
-              <option value="green"></option>
-              <option value="blue"></option>
-              <option value="orange"></option>
-              <option value="violet"></option>
-              <option value="#d0d1d2"></option>
-              <option selected></option>
-            </select>
-            <button className="ql-link"></button>
-            <button className="ql-image"></button>
-            <button className="ql-video"></button>
-
-            <button className="ql-blockquote"></button>
-            <button className="ql-code-block"></button>
-          </div>
+          <Toolbar/>
           <div className="actions pull-right">
             <Button className="save-btn" type="primary">Guardar</Button>
             <Icon className="close-icon" type="close" />
---
+++ src/components/Toolbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Toolbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id='toolbar'>
+        <select className="ql-header">
+          <option value="1"/>
+          <option value="2"></option>
+          <option selected></option>
+        </select>
+        <select className="ql-align">
+          <option value="center"></option>
+          <option value="right"></option>
+          <option value="justify"></option>
+          <option selected></option>
+        </select>
+        <button className="ql-bold"></button>
+        <button className="ql-italic"></button>
+        <button className="ql-underline"></button>
+        <button className="ql-strike"></button>
+        <select className="ql-color">
+          <option value="red"></option>
+          <option value="green"></option>
+          <option value="blue"></option>
+          <option value="orange"></option>
+          <option value="violet"></option>
+          <option value="#d0d1d2"></option>
+          <option selected></option>
+        </select>
+        <button className="ql-link"></button>
+        <button className="ql-image"></button>
+        <button className="ql-video"></button>
+
+        <button className="ql-blockquote"></button>
+        <button className="ql-code-block"></button>
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
